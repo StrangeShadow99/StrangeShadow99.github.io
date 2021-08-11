11da2983990a7c28516b9cb8a7644a295af8b29f
NickolasKaley <nkaley99@gmail.com>
2021-08-10 21:13:43 -0400

    alot

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 38.996081, lng: -84.644628},
@@ -16,3 +17,49 @@ function initMap() {
       }
   });
 } 
+
+var i = 0;
+var txt = 'Nickolas';
+var txtTwo= 'Kaley';
+var speed = 200;
+var next = false;
+function typeW() {
+  if (i < txt.length && next != true) {
+    document.getElementById("Nick").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeW, speed);
+  } 
+  if(i == txt.length){
+    i=0;
+    next= true;
+    typeWT();
+  }
+}
+  
+function typeWT(){
+  if (i < txtTwo.length) {
+    document.getElementById("Kaley").innerHTML += txtTwo.charAt(i);
+    i++;
+    setTimeout(typeWT, speed);
+  }
+}
+  
+
+
+function sendEmail() {
+  let name = document.querySelector("#NameInput").value;
+  let email = document.querySelector("#EmailInput").value;
+  let message = document.querySelector("#MessageInput").value;
+  let subject = document.querySelector("#SubjectInput").value;
+
+  Email.send({
+  SecureToken : "79de3fcb-050b-47eb-9440-ca21229f23aa",
+  To : 'nkaley99@gmail.com',
+  From : "nkaley99@gmail.com",
+  Subject: `${subject}`,
+  Body: `Name: ${name} <br/> Email: ${email} <br/> Message: ${message}`,
+  })
+  .then(function (message) {
+  alert("Mail has been sent successfully")
+  });
+  }
